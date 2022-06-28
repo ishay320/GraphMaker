fd126ed6ac5330aa4ea133383a8b2da8abf793be
ishay320 <ishay320@hotmail.com>
2022-06-28 23:44:50 +0300

    get files from folder by input

--- main.cpp
+++ main.cpp
@@ -1,6 +1,65 @@
+#include <filesystem>
 #include <iostream>
+#include <string>
+#include <vector>
 
-int main(int argc, char const *argv[]) {
-    std::cout << "hello world\n";
+std::vector<std::filesystem::directory_entry> getFilesByFilter(const std::string path, const std::vector<std::string> extensions,
+                                                               const std::vector<std::string> excludes = {})
+{
+    std::vector<std::filesystem::directory_entry> files;
+    std::filesystem::recursive_directory_iterator rdi(path);
+    for (const std::filesystem::directory_entry& entry : rdi)
+    {
+        if (entry.is_directory())
+        {
+            for (auto&& exclude : excludes)
+            {
+                if (entry.path().filename() == exclude)
+                {
+                    rdi.disable_recursion_pending(); // don't recurse into this directory.
+                }
+            }
+        }
+        else if (entry.is_regular_file())
+        {
+            for (const std::string& extension : extensions)
+            {
+                if (entry.path().filename().extension() == extension)
+                {
+                    files.push_back(entry);
+                }
+            }
+        }
+    }
+
+    return files;
+}
+
+int main(int argc, char const* argv[])
+{
+    // Input parsing
+    if (argc < 2)
+    {
+        std::cout << "Usage: " << argv[0] << " <path> [exclude list]\n";
+        return 1;
+    }
+
+    std::string path = argv[1];
+
+    std::vector<std::string> excludes;
+    for (size_t i = 2; i < argc; i++)
+    {
+        excludes.emplace_back(argv[i]);
+    }
+
+    std::vector<std::string> extensions{".cpp", ".hpp", ".c", ".h"};
+
+    std::vector<std::filesystem::directory_entry> files = getFilesByFilter(path, extensions, excludes);
+
+    for (auto&& file : files)
+    {
+        std::cout << file.path() << '\n';
+    }
+
     return 0;
 }
